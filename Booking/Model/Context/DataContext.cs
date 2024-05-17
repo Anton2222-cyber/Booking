6d117c059fd8daa044d5b0c7230c1d4ca18cb4b7
Приходько Владислав <3885vlad@gmail.com>
2024-05-17 19:58:01 +0300

    Created database context

---
+++ Booking/Model/Context/DataContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Model.Entities;
+using Model.EntityTypeConfigurations;
+
+namespace Model.Context;
+
+public class DataContext(DbContextOptions<DataContext> options)
+	: DbContext(options) {
+
+	public DbSet<Country> Countries { get; set; }
+	public DbSet<City> Cities { get; set; }
+	public DbSet<Address> Addresses { get; set; }
+	public DbSet<Hotel> Hotels { get; set; }
+	public DbSet<HotelPhoto> HotelPhotos { get; set; }
+
+	protected override void OnModelCreating(ModelBuilder modelBuilder) {
+		base.OnModelCreating(modelBuilder);
+
+		new CountryEntityTypeConfiguration().Configure(modelBuilder.Entity<Country>());
+		new CityEntityTypeConfiguration().Configure(modelBuilder.Entity<City>());
+		new AddressEntityTypeConfiguration().Configure(modelBuilder.Entity<Address>());
+		new HotelEntityTypeConfiguration().Configure(modelBuilder.Entity<Hotel>());
+		new HotelPhotoEntityTypeConfiguration().Configure(modelBuilder.Entity<HotelPhoto>());
+	}
+}
