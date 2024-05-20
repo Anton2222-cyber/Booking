09338e27c81d63c4641598513bc4019c08f5346d
Приходько Владислав <3885vlad@gmail.com>
2024-05-20 22:50:18 +0300

    Installed package and created identity entities

---
+++ Booking/Model/Entities/Identity/User.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Model.Entities.Identity;
+
+public class User : IdentityUser<long> {
+	public string FirstName { get; set; } = null!;
+
+	public string LastName { get; set; } = null!;
+
+	public string Photo { get; set; } = null!;
+
+	public virtual ICollection<UserRole> UserRoles { get; set; } = null!;
+}
