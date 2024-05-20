5a95c443b3499ce16fa372556eaa100936eddda8
Приходько Владислав <3885vlad@gmail.com>
2024-05-20 23:18:17 +0300

    Added AccountsController

---
+++ Booking/Booking/Controllers/AccountsController.cs
@@ -0,0 +1,70 @@
+﻿using AutoMapper;
+using Booking.Constants;
+using Booking.Services;
+using Booking.Services.Interfaces;
+using Booking.ViewModels.Account;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Model.Entities.Identity;
+
+namespace Booking.Controllers;
+
+[Route("api/[controller]/[action]")]
+[ApiController]
+public class AccountsController(
+	UserManager<User> userManager,
+	IJwtTokenService jwtTokenService,
+	IMapper mapper,
+	ImageService imageService,
+	IConfiguration configuration
+	) : ControllerBase {
+
+	[HttpPost]
+	public async Task<IActionResult> SignIn([FromForm] SignInVm model) {
+		User? user = await userManager.FindByEmailAsync(model.Email);
+
+		if (user is null || !await userManager.CheckPasswordAsync(user, model.Password))
+			return Unauthorized("Wrong authentication data");
+
+		return Ok(new JwtTokenResponse {
+			Token = await jwtTokenService.CreateTokenAsync(user)
+		});
+	}
+
+	[HttpPost]
+	public async Task<IActionResult> Registration([FromForm] RegisterVm model) {
+		if (await userManager.FindByEmailAsync(model.Email) is not null)
+			return BadRequest("The user with this email is already registered");
+
+		User user = mapper.Map<RegisterVm, User>(model);
+
+		try {
+			user.Photo = await imageService.SaveImageAsync(model.Image);
+		}
+		catch (Exception) {
+			return BadRequest("Image error");
+		}
+
+		try {
+			IdentityResult identityResult = await userManager.CreateAsync(user, model.Password);
+			if (!identityResult.Succeeded)
+				return BadRequest(identityResult.Errors.First().Description);
+
+			identityResult = await userManager.AddToRoleAsync(user, Roles.User);
+
+			if (!identityResult.Succeeded) {
+				await userManager.DeleteAsync(user);
+				imageService.DeleteImageIfExists(user.Photo);
+				return BadRequest("Role assignment error");
+			}
+
+			return Ok(new JwtTokenResponse {
+				Token = await jwtTokenService.CreateTokenAsync(user)
+			});
+		}
+		catch {
+			imageService.DeleteImageIfExists(user.Photo);
+			throw;
+		}
+	}
+}
---
+++ Booking/Booking/Services/JwtTokenService.cs
@@ -0,0 +1,60 @@
+﻿using Booking.Services.Interfaces;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.IdentityModel.Tokens;
+using Model.Entities.Identity;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace Booking.Services;
+
+public class JwtTokenService(
+	UserManager<User> userManager,
+	IConfiguration configuration
+	) : IJwtTokenService {
+
+	public async Task<string> CreateTokenAsync(User user) {
+		var key = Encoding.UTF8.GetBytes(
+			configuration["Authentication:Jwt:SecretKey"]
+				?? throw new NullReferenceException("Authentication:Jwt:SecretKey")
+		);
+
+		int tokenLifetimeInDays = Convert.ToInt32(
+			configuration["Authentication:Jwt:TokenLifetimeInDays"]
+				?? throw new NullReferenceException("Authentication:Jwt:TokenLifetimeInDays")
+		);
+
+		var signinKey = new SymmetricSecurityKey(key);
+
+		var signinCredential = new SigningCredentials(signinKey, SecurityAlgorithms.HmacSha256);
+
+		var jwt = new JwtSecurityToken(
+			signingCredentials: signinCredential,
+			expires: DateTime.Now.AddDays(tokenLifetimeInDays),
+			claims: await GetClaimsAsync(user));
+
+		return new JwtSecurityTokenHandler().WriteToken(jwt);
+	}
+
+	private async Task<List<Claim>> GetClaimsAsync(User user) {
+		string userEmail = user.Email
+			?? throw new NullReferenceException($"User.Email");
+
+		var userRoles = await userManager.GetRolesAsync(user);
+
+		var roleClaims = userRoles
+			.Select(r => new Claim(ClaimTypes.Role, r))
+			.ToList();
+
+		var claims = new List<Claim> {
+			new ("email", userEmail),
+			new ("firstName", user.FirstName),
+			new ("lastName", user.LastName),
+			new ("age", user.Age.ToString()),
+			new ("photo", user.Photo)
+		};
+		claims.AddRange(roleClaims);
+
+		return claims;
+	}
+}
