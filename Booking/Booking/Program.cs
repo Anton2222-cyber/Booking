ad4eb7cffa4555be77c0a71f6aac851ca35f54a2
Приходько Владислав <3885vlad@gmail.com>
2024-05-25 21:14:04 +0300

    Refactoring of AccountsController

--- Booking/Booking/Program.cs
+++ Booking/Booking/Program.cs
@@ -115,6 +115,8 @@ builder.Services.AddTransient<IImageValidator, ImageValidator>();
 builder.Services.AddTransient<IIdentityService, IdentityService>();
 builder.Services.AddTransient<IExistingEntityCheckerService, ExistingEntityCheckerService>();
 
+builder.Services.AddTransient<IAccountsControllerService, AccountsControllerService>();
+
 builder.Services.AddTransient<ICountriesControllerService, CountriesControllerService>();
 builder.Services.AddTransient<IPaginationService<CountryVm, CountryFilterVm>, CountryPaginationService>();
 
