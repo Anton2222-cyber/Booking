bf5589fd64790152aa33aaa65cbc0995ca5815c4
Приходько Владислав <3885vlad@gmail.com>
2024-05-25 14:32:22 +0300

    Added new entities

---
+++ Booking/Model/Entities/Room.cs
@@ -0,0 +1,15 @@
+﻿namespace Model.Entities;
+
+public class Room {
+	public long Id { get; set; }
+
+	public string Name { get; set; } = null!;
+
+	public decimal Price { get; set; }
+
+	public int AdultPlaces { get; set; }
+
+	public int ChildrenPlaces { get; set; }
+
+	public ICollection<RoomConvenience> Conveniences { get; set; } = null!;
+}
