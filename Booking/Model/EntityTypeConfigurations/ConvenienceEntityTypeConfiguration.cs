9d2db5a5e54cd828ecaef2d78149d486e008d0bf
Приходько Владислав <3885vlad@gmail.com>
2024-05-25 15:04:25 +0300

    Added entity type configurations. Added DbSets to context

---
+++ Booking/Model/EntityTypeConfigurations/ConvenienceEntityTypeConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Model.Entities;
+
+namespace Model.EntityTypeConfigurations;
+
+internal class ConvenienceEntityTypeConfiguration : IEntityTypeConfiguration<Convenience> {
+	public void Configure(EntityTypeBuilder<Convenience> builder) {
+		builder.ToTable("Conveniences");
+
+		builder.Property(c => c.Name)
+			.HasMaxLength(255)
+			.IsRequired();
+	}
+}
