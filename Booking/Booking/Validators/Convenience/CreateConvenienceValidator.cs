4f85aa9c4930e4d3b40bcdefcfb83a3b5204fad5
Приходько Владислав <3885vlad@gmail.com>
2024-05-27 13:13:55 +0300

    Refactoring

--- Booking/Booking/Validators/Convenience/CreateConvenienceValidator.cs
+++ Booking/Booking/Validators/Convenience/CreateConvenienceValidator.cs
@@ -6,7 +6,9 @@ namespace Booking.Validators.Convenience;
 public class CreateConvenienceValidator : AbstractValidator<CreateConvenienceVm> {
 	public CreateConvenienceValidator() {
 		RuleFor(c => c.Name)
-			.NotEmpty().WithMessage("Name is empty or null")
-			.MaximumLength(255).WithMessage("Name is too long");
+			.NotEmpty()
+				.WithMessage("Name is empty or null")
+			.MaximumLength(255)
+				.WithMessage("Name is too long");
 	}
 }
--- Booking/Booking/Validators/Convenience/UpdateConvenienceValidator.cs
+++ Booking/Booking/Validators/Convenience/UpdateConvenienceValidator.cs
@@ -8,10 +8,12 @@ public class UpdateConvenienceValidator : AbstractValidator<UpdateConvenienceVm>
 	public UpdateConvenienceValidator(IExistingEntityCheckerService existingEntityCheckerService) {
 		RuleFor(c => c.Id)
 			.MustAsync(existingEntityCheckerService.IsCorrectConvenienceId)
-			.WithMessage("Convenience with this id is not exists");
-		
+				.WithMessage("Convenience with this id is not exists");
+
 		RuleFor(c => c.Name)
-			.NotEmpty().WithMessage("Name is empty or null")
-			.MaximumLength(255).WithMessage("Name is too long");
+			.NotEmpty()
+				.WithMessage("Name is empty or null")
+			.MaximumLength(255)
+				.WithMessage("Name is too long");
 	}
 }
