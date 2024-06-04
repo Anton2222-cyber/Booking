ee4c4a510db4a7cc00ccb4004335a7dcf6e53953
Приходько Владислав <3885vlad@gmail.com>
2024-06-04 22:48:12 +0300

    Added validators

--- Booking/Booking/Services/Interfaces/IExistingEntityCheckerService.cs
+++ Booking/Booking/Services/Interfaces/IExistingEntityCheckerService.cs
@@ -1,7 +1,9 @@
 ﻿namespace Booking.Services.Interfaces;
 
 public interface IExistingEntityCheckerService {
+	Task<bool> IsCorrectCountryId(long id, CancellationToken cancellationToken);
 	Task<bool> IsCorrectHotelId(long id, CancellationToken cancellationToken);
+	Task<bool> IsCorrectHotelTypeId(long id, CancellationToken cancellationToken);
 	Task<bool> IsCorrectHotelReviewId(long id, CancellationToken cancellationToken);
 	Task<bool> IsCorrectConvenienceId(long id, CancellationToken cancellationToken);
 	Task<bool> IsCorrectConvenienceIds(IEnumerable<long>? ids, CancellationToken cancellationToken);
