43a394997fe302e6be85d6bf0c296fa554c58ba9
Приходько Владислав <3885vlad@gmail.com>
2024-06-08 19:57:08 +0300

    Added FavoriteHotelEntityTypeConfiguration

---
+++ Booking/Model/EntityTypeConfigurations/FavoriteHotelEntityTypeConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Model.Entities;
+
+namespace Model.EntityTypeConfigurations;
+
+internal class FavoriteHotelEntityTypeConfiguration : IEntityTypeConfiguration<FavoriteHotel> {
+	public void Configure(EntityTypeBuilder<FavoriteHotel> builder) {
+		builder.ToTable("FavoriteHotels");
+
+		builder.HasKey(fh => new {
+			fh.HotelId,
+			fh.UserId
+		});
+	}
+}
