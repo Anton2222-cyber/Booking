dcc04080848818dab8f6959618a627b7782d7a24
Приходько Владислав <3885vlad@gmail.com>
2024-06-08 20:00:45 +0300

    Added migration

---
+++ Booking/Booking/Migrations/20240608170004_Added_FavoriteHotels_table.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Booking.Migrations
+{
+    /// <inheritdoc />
+    public partial class Added_FavoriteHotels_table : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "FavoriteHotels",
+                columns: table => new
+                {
+                    HotelId = table.Column<long>(type: "bigint", nullable: false),
+                    UserId = table.Column<long>(type: "bigint", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_FavoriteHotels", x => new { x.HotelId, x.UserId });
+                    table.ForeignKey(
+                        name: "FK_FavoriteHotels_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_FavoriteHotels_Hotels_HotelId",
+                        column: x => x.HotelId,
+                        principalTable: "Hotels",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_FavoriteHotels_UserId",
+                table: "FavoriteHotels",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "FavoriteHotels");
+        }
+    }
+}
