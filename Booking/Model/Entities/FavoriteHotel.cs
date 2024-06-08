d772412ff09e1e234b4bf354109c44a49e466341
Приходько Владислав <3885vlad@gmail.com>
2024-06-08 19:55:56 +0300

    Added FavoriteHotel entity

---
+++ Booking/Model/Entities/FavoriteHotel.cs
@@ -0,0 +1,11 @@
+﻿using Model.Entities.Identity;
+
+namespace Model.Entities;
+
+public class FavoriteHotel {
+	public long HotelId { get; set; }
+	public Hotel Hotel { get; set; } = null!;
+
+	public long UserId { get; set; }
+	public User User { get; set; } = null!;
+}
