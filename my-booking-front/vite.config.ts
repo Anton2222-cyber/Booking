7f698bfa9e02076af0e3c56776c2f1434214865e
ihorSamolich <126615259+ihorSamolich@users.noreply.github.com>
2024-06-14 12:38:04 +0300

    fix build errors

--- my-booking-front/vite.config.ts
+++ my-booking-front/vite.config.ts
@@ -25,10 +25,10 @@ export default defineConfig({
             interfaces: "/src/interfaces",
         },
     },
-    // server: {
-    //     port: 5173,
-    //     strictPort: true,
-    //     host: true,
-    //     origin: "http://0.0.0.0:5173",
-    // },
+    server: {
+        port: 5173,
+        strictPort: true,
+        host: true,
+        origin: "http://0.0.0.0:5173",
+    },
 });
