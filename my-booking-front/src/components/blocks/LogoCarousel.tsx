d88572280a307cdf8a8d633baff94f1e2228f73f
ihorSamolich <126615259+ihorSamolich@users.noreply.github.com>
2024-06-15 17:41:02 +0300

    update carousel add hover effect

--- my-booking-front/src/components/blocks/LogoCarousel.tsx
+++ my-booking-front/src/components/blocks/LogoCarousel.tsx
@@ -6,7 +6,7 @@ import logo5 from "assets/footer05.png";
 import { animate, motion, useMotionValue } from "framer-motion";
 import useMeasure from "react-use-measure";
 
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 const logos = [
     logo1,
@@ -30,25 +30,52 @@ const logos = [
 
 const LogoCarousel = () => {
     const [ref, { width }] = useMeasure();
+    const [duration, setDuration] = useState<number>(40);
+    const [mustFinish, setMustFinish] = useState<boolean>(false);
+    const [rerender, setRerender] = useState<boolean>(false);
+
     const xTransition = useMotionValue(0);
 
     useEffect(() => {
         let controls;
         let finalPosition = -width / 2 - 8;
 
-        controls = animate(xTransition, [0, finalPosition], {
-            ease: "linear",
-            repeat: Infinity,
-            duration: 40,
-            repeatType: "loop",
-            repeatDelay: 0,
-        });
+        if (mustFinish) {
+            controls = animate(xTransition, [xTransition.get(), finalPosition], {
+                ease: "linear",
+                duration: duration * (1 - xTransition.get() / finalPosition),
+                onComplete: () => {
+                    setMustFinish(false);
+                    setRerender(!rerender);
+                },
+            });
+        } else {
+            controls = animate(xTransition, [0, finalPosition], {
+                ease: "linear",
+                repeat: Infinity,
+                duration: duration,
+                repeatType: "loop",
+                repeatDelay: 0,
+            });
+        }
 
-        return controls.stop;
-    }, [xTransition, width]);
+        return controls?.stop;
+    }, [xTransition, width, duration, rerender]);
 
     return (
-        <motion.div className="absolute left-0 flex gap-4" style={{ x: xTransition }} ref={ref}>
+        <motion.div
+            className="absolute left-0 flex gap-4"
+            style={{ x: xTransition }}
+            ref={ref}
+            onHoverStart={() => {
+                setMustFinish(true);
+                setDuration(80);
+            }}
+            onHoverEnd={() => {
+                setMustFinish(true);
+                setDuration(40);
+            }}
+        >
             {[...logos, ...logos].map((image, index) => (
                 <div
                     key={index}
